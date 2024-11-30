b962ee61f6a4623b40216fbe02e5658a9018c1f0
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-30 21:45:29 +0800

    remove the middlewater

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,3 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
 
-Route::middleware('auth:passport')->get('/checktoken', [WebController::class, 'checkToken'])->name('checktoken');
